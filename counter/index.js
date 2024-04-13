66004a5ded0366e0aacf9c9c728f027977bffb41
forfd8960 <forfd8960@gmail.com>
2024-04-13 23:14:48 +0800

    Increase count and display it after click button

--- counter/index.js
+++ counter/index.js
@@ -1,1 +1,16 @@
-document.getElementById("counter-el").innerText = 100;
+// document.getElementById("counter-el").innerText = 100;
+
+let count = 0;
+
+setCounterValue(count);
+
+function increment() {
+  console.log("the button is clicked");
+  count += 1;
+  console.log("count: ", count);
+  setCounterValue(count);
+}
+
+function setCounterValue(n) {
+  document.getElementById("counter-el").innerText = n;
+}
